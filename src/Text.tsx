a55f1889dc11c9a1bc7a62b775401cfb333e95c2
joeyst <joeystenbeck@gmail.com>
2022-05-31 19:38:32 -0700

    Add text checker, remove focus outline, add inline CSS, add keyboard listener.

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { KeyboardEvent, useState } from "react";
 
 function GetText() {
-  return <h1> This is some text. </h1>
+  const [typed, setTyped] = useState("");
+
+  const textToType = "abcde"
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    setTyped(typed + event.key)
+  }
+
+  const handleBackspace = (event: KeyboardEvent) => {
+    if (typed !== "" && event.key === "Backspace") {
+      setTyped(typed.slice(0,-1))
+      console.log(event.keyCode)
+    }
+  }
+
+  function getNumMatched(desiredText: String, actualText: String): number {
+    let numMatched = 0
+    for (let i = 0; i < desiredText.length; i++) {
+      if (desiredText[i] === actualText[i]) {
+        numMatched += 1
+      }
+      else {
+        return numMatched
+      }
+    }
+    return numMatched
+  }
+
+  function getNumIncorrect(desiredText: String, actualText: String) {
+    return (actualText.length - getNumMatched(desiredText, actualText))
+  }
+
+  function comparedText(desiredText: String, actualText: String) {
+    const numMatched = getNumMatched(desiredText, actualText)
+    const numIncorrect = getNumIncorrect(desiredText, actualText)
+    const textMatched = actualText.slice(0, Math.max(0, numMatched))
+    const textMismatched = actualText.slice(Math.max(numMatched), Math.max(numMatched)+numIncorrect)
+    return <div> <p style={{ color: 'green' }}> {textMatched} </p><p style={{ color: 'red' }}> {textMismatched} </p> </div>
+  }
+
+  return (
+    <div tabIndex={1}
+    onFocus={(e) => {
+      console.log("clicked")
+    }}
+    onKeyPress={handleKeyPress} 
+    onKeyDown={handleBackspace}
+    style={{outline: 0}}
+    >
+      <p> Type: {textToType} </p>
+      {comparedText(textToType, typed)}
+    </div>
+  )
+
 }
 
 export default GetText;
